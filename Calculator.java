921af5adc3bcfb3c6d485ee9b127412d77263f68
Juan Carlos Velásquez <velasquez.gaviria@.gmail.com>
2022-09-25 16:51:22 -0500

    Added substraction method

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,13 @@ public class Calculator() {
     public double addition(double a, double b) {
         return b + a;
     }
+
+    public int substraction(int a, int b( {
+        return a - b;
+    }
+
+    public double substraction(double a, double b) {
+        return a - b;
+    }
 }
 
